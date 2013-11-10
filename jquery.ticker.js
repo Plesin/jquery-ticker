3621040de4b699c9025c2fca5f2edd56623e85f3
Radek <raduz74@gmail.com>
2013-11-10 18:09:00 +0100

    remove trailing white space

--- jquery.ticker.js
+++ jquery.ticker.js
@@ -9,50 +9,50 @@
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * Examples http://www.radekpleskac.com.com/projects/jquery-ticker/	
- * jQuery plugin to turn an unordered list <ul> into a simple ticker, displaying one list item at a time. 
+ * Examples http://www.radekpleskac.com.com/projects/jquery-ticker/
+ * jQuery plugin to turn an unordered list <ul> into a simple ticker, displaying one list item at a time.
  *
 */
 
 (function($){
 
 	$.fn.ticker = function(options) {
-		
+
 		$.fn.ticker.defaults =  {
 			controls: false, //show controls, to be implemented
 			interval: 3000, //interval to show next item
 			effect: "fadeIn", // available effects: fadeIn, slideUp, slideDown
 			duration: 400 //duration of the change to the next item
 		};
-		
+
 		var o = $.extend({}, $.fn.ticker.defaults, options);
-		
+
 		if (!this.length)
-			return;				
-		
+			return;
+
 		return this.each(function() {
-			
-			var $ul = $(this),				
+
+			var $ul = $(this),
 				$items = $ul.find("li"),
 				index = 0,
 				paused = false,
 				time;
-			
+
 			function start() {
-				time = setInterval(function() {					
+				time = setInterval(function() {
 					if (!paused)
-						changeItem();																					
+						changeItem();
 				}, o.interval);
 			}
-			
+
 			function changeItem() {
-				
+
 				var $current = $items.eq(index);
 				index++;
 				if (index == $items.length)
 					index = 0;
-				var $next =  $items.eq(index);					   
-								
+				var $next =  $items.eq(index);
+
 				if (o.effect == "fadeIn") {
 					$current.fadeOut(function() {
 						$next.fadeIn();
@@ -64,40 +64,40 @@
 					$current.animate({
 						top: -h * d + "px"
 					}, o.duration, function() { $(this).hide(); });
-					$next.css({ 
+					$next.css({
 						"display": "block",
 						"top": h * d + "px"
 					});
 					$next.animate({
 						top: 0
-					}, o.duration);				
+					}, o.duration);
 				}
-				
+
 			}
-			
+
 			function bindEvents() {
 				$ul.hover(function() {
 					paused = true;
-				},function() {					
+				},function() {
 					paused = false;
 				});
 			}
-												
+
 			function init() {
 				$items.not(":first").hide();
 				if (o.effect == "slideUp" || o.effect == "slideDown") {
 					$ul.css("position", "relative");
 					$items.css("position", "absolute");
 				}
-					
+
 				bindEvents();
 				start();
-			}			
-			
+			}
+
 			init();
-											
-		});	
-							
+
+		});
+
 	};
-		
+
 })(jQuery);
